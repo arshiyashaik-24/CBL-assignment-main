3b60a0f658a5892741341069423b6bb9dd9c3b8e
PriyanshuGuha <p.guha@student.tue.nl>
2025-10-23 11:15:33 +0200

    Shortened code in Minigame.java

--- Minigame.java
+++ Minigame.java
@@ -61,19 +61,13 @@ public class Minigame extends JPanel implements ActionListener, KeyListener {
     }
     
     @Override
-    public void keyPressed(KeyEvent e) {
-        
-    }
+    public void keyPressed(KeyEvent e) {}
     
     @Override
-    public void keyReleased(KeyEvent e) {
-        
-    }
+    public void keyReleased(KeyEvent e) {}
     
     @Override
-    public void keyTyped(KeyEvent e) {
-        
-    }
+    public void keyTyped(KeyEvent e) {}
     
     @Override
     public void actionPerformed(ActionEvent e) {
